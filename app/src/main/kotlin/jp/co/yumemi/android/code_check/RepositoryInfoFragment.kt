0c1f28081976c9521fccfe326631c6e169116a30
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-15 11:00:49 +0900

    Fragment, ViewModelを責務がわかるような名前にrename TwoFragment -> RepositoryInfoFragmentに修正、それに付随するViewModelも修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryInfoFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/RepositoryInfoFragment.kt
@@ -10,13 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import coil.load
 import jp.co.yumemi.android.code_check.TopActivity.Companion.lastSearchDate
-import jp.co.yumemi.android.code_check.databinding.FragmentTwoBinding
+import jp.co.yumemi.android.code_check.databinding.FragmentRepositoryInfoBinding
 
-class TwoFragment : Fragment(R.layout.fragment_two) {
+class RepositoryInfoFragment : Fragment(R.layout.fragment_repository_info) {
 
-    private val args: TwoFragmentArgs by navArgs()
+    private val args: RepositoryInfoFragmentArgs by navArgs()
 
-    private var binding: FragmentTwoBinding? = null
+    private var binding: FragmentRepositoryInfoBinding? = null
     private val _binding get() = binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,7 +24,7 @@ class TwoFragment : Fragment(R.layout.fragment_two) {
 
         Log.d("検索した日時", lastSearchDate.toString())
 
-        binding = FragmentTwoBinding.bind(view)
+        binding = FragmentRepositoryInfoBinding.bind(view)
 
         val item = args.item
 
