5acaf7397d5ab187f42b2aa88f430adb3b39885b
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-17 08:28:40 +0900

    直感的にわかりづらかったためコメントを記載

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
@@ -3,6 +3,7 @@
  */
 package jp.co.yumemi.android.code_check
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import jp.co.yumemi.android.code_check.network.HttpClientSingleton.client
 import java.util.Date
@@ -19,9 +20,17 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         var lastSearchDate: Date? = null
     }
 
+    /**
+     * 下記のタイミングで呼び出される
+     * 1. アプリケーションがユーザーによって終了される場合。
+     * 2. システムによってリソースの解放が必要な場合。
+     * 3. アクティビティのライフサイクルの一部として、設定変更（例: 画面の回転）や他のアクティビティへの遷移時。
+     */
     override fun onDestroy() {
         super.onDestroy()
-        // Activityが破棄される際に、HttpClientを閉じる
+
+        // アプリケーションが終了する際に、HttpClientをクローズする
+        // 画面の回転時もこの関数自体は呼ばれるため、isFinishingでユーザーがアプリを終了したかどうかを判定する
         if (isFinishing) {
             client.close()
         }
