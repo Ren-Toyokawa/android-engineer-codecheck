cba0c1434d19d3d6d83f4ae0c8ce5a056bb54137
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-16 10:38:37 +0900

    lateinitを除去

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/MainActivity.kt
@@ -11,9 +11,10 @@ import java.util.Date
  * このアクティビティが本アプリのエントリーポイントとなる
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    // FIXME: Jetpack Datastoreなどを使用した方が良い気がする。
     companion object {
         // 最後に検索した日時
         // 検索を実行した際に、この変数に現在時刻を代入する
-        lateinit var lastSearchDate: Date
+        var lastSearchDate: Date? = null
     }
 }
