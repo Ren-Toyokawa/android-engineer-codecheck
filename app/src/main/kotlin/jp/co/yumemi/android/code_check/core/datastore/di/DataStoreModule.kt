acd37527ca098e9eaa8ccd75c276d4bc7186b04b
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-17 18:07:18 +0900

    - Datastoreを使用するための 依存の追加 - DatastoreModuleの定義

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/core/datastore/di/DataStoreModule.kt
@@ -0,0 +1,24 @@
+package jp.co.yumemi.android.code_check.core.datastore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            produceFile = { context.preferencesDataStoreFile("settings") }
+        )
+    }
+}
