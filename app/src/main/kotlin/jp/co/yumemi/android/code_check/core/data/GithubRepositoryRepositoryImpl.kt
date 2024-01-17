1a6ea2a7c5b72816c608aaec24cfe7481ed275c2
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-17 16:45:00 +0900

    HttpClientをDIするように

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/core/data/GithubRepositoryRepositoryImpl.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/core/data/GithubRepositoryRepositoryImpl.kt
@@ -1,21 +1,23 @@
 package jp.co.yumemi.android.code_check.core.data
 
+import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.request.parameter
 import jp.co.yumemi.android.code_check.core.data.model.RepositorySearchResponse
 import jp.co.yumemi.android.code_check.core.data.model.toExternalModel
 import jp.co.yumemi.android.code_check.core.model.GithubRepositorySummary
-import jp.co.yumemi.android.code_check.network.HttpClientSingleton.client
 import javax.inject.Inject
 
 /**
  * Githubのレポジトリにまつわるデータを取得する
  */
-class GithubRepositoryRepositoryImpl @Inject constructor(): GithubRepositoryRepository {
+class GithubRepositoryRepositoryImpl @Inject constructor(
+    private val httpClient: HttpClient
+): GithubRepositoryRepository {
     override suspend fun searchRepository(query: String): List<GithubRepositorySummary> {
         val response: RepositorySearchResponse =
-            client.get("https://api.github.com/search/repositories") {
+            httpClient.get("https://api.github.com/search/repositories") {
             header("Accept", "application/vnd.github.v3+json")
             parameter("q", query)
         }
