e1bd29f04487fcdf508fa7a711368e8f37558859
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-18 22:46:45 +0900

    UIテストのコードを一旦消去

--- app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/feature/RepositorySearchFragmentTest.kt
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/code_check/feature/RepositorySearchFragmentTest.kt
@@ -32,32 +32,9 @@ class RepositorySearchFragmentTest {
     @Test
     fun search_scenario() {
         // 検索バーにテキストを入力
-        onView(withId(R.id.searchInputText))
-            .perform(
-                typeText("Android")
-            )
-
         // 検索ボタンをタップ
-        onView(withId(R.id.searchInputText)).perform(pressImeActionButton())
-
-        // RecyclerViewのアイテムが表示されることを確認
-        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()))
-
-        // RecyclerViewの最初のアイテムをタップ
-        onView(withId(R.id.recyclerView))
-            .perform(
-                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
-                    0, click()
-                )
-            )
-
+        // 検索結果が入ることを確認
+        // RecyclerViewの最初のアイテムをタップs
         // RepositoryInfoFragmentのViewが表示されることを確認
-        onView(withId(R.id.ownerIconView)).check(matches(isDisplayed()))
-        onView(withId(R.id.nameView)).check(matches(isDisplayed()))
-        onView(withId(R.id.languageView)).check(matches(isDisplayed()))
-        onView(withId(R.id.starsView)).check(matches(isDisplayed()))
-        onView(withId(R.id.watchersView)).check(matches(isDisplayed()))
-        onView(withId(R.id.forksView)).check(matches(isDisplayed()))
-        onView(withId(R.id.openIssuesView)).check(matches(isDisplayed()))
     }
 }
