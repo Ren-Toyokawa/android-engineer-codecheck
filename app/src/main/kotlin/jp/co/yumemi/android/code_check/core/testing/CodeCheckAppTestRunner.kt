1f3785ff89170a1cfd4be0426c6faaed080a605c
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-18 12:15:30 +0900

    テスト時にFakeを見るうに下記を修正 - Fakeの Repository class - Fakeの Module クラス - Hiltの設定

---
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/core/testing/CodeCheckAppTestRunner.kt
@@ -0,0 +1,12 @@
+package jp.co.yumemi.android.code_check.core.testing
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
+
+class CodeCheckAppTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, name: String?, context: Context?): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
