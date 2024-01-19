b0ee46e11100b9bf62d249cf2de7cc3a1ad5b912
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-19 11:34:47 +0900

    testTagを追加

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/feature/repository/search/RepositoryInfoScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/feature/repository/search/RepositoryInfoScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,12 +46,13 @@ fun RepositoryInfoRoute(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RepositoryInfoScreen(
-    modifier: Modifier = Modifier,
     repositorySummary: GithubRepositorySummary
 ) {
     val scrollState = rememberScrollState()
 
-    Scaffold {
+    Scaffold(
+        modifier = Modifier.testTag("RepositoryInfoScreen")
+    ) {
         Column(
             modifier = Modifier
                 .verticalScroll(scrollState)
