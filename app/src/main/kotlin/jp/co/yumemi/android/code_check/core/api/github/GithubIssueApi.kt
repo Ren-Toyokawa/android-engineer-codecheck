cf73c137fd6cf8f2d45afea0cee4fe11075538af
Ren-Toyokawa <rentoytoy0828@gmail.com>
2024-01-19 14:57:23 +0900

    Responseの形式を間違えていたため修正

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/core/api/github/GithubIssueApi.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/core/api/github/GithubIssueApi.kt
@@ -4,17 +4,16 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import jp.co.yumemi.android.code_check.core.data.model.Issue
-import jp.co.yumemi.android.code_check.core.data.model.IssueResponse
 import javax.inject.Inject
 
 class GithubIssueApi @Inject constructor(
     private val httpClient: HttpClient,
 ) {
     suspend fun getIssueList(owner: String, repo: String): List<Issue> {
-        val response: IssueResponse =
+        val response: List<Issue> =
             httpClient.get("https://api.github.com/repos/$owner/$repo/issues") {
                 header("Accept", "application/vnd.github.v3+json")
             }
-        return response.items
+        return response
     }
 }
